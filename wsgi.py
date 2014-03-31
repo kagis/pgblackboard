b7ca9e4d08298f7b0f6a7ca1d2c19856926eb270
exe-dealer <exe-dealer@yandex.ru>
2014-03-31 15:45:41 +0600

    oop renderers

--- wsgi.py
+++ wsgi.py
@@ -103,179 +103,226 @@ def application(environ):
             ['Invalid user name or password.']
         )
 
-    resp_func, content_type = {
-        'map': (map_response, 'text/html'),
-        'html': (html_response, 'text/html'),
-        'json': (json_response, 'application/json'),
-    }[format or 'html']
+    renderer = RENDERERS.get(format, RegularRenderer)
     return ('200 OK',
-        [('Content-type', content_type),
+        [('Content-type', renderer.mime_type),
          ('uWSGI-Encoding', 'gzip')],
-        process_sql(conn, resp_func, query, args)
+        process_sql(conn, query, args, renderer)
     )
 
 
-def process_sql(conn, resp_func, query, args):
+def process_sql(conn, query, args, renderer):
     statements = (
-        (lambda: stmt.chunks(*args)
-            if stmt.column_names
-            else stmt.first(*args),
-        stmt.column_names,
-        stmt.pg_column_types)
-        for stmt in (
-            conn.prepare(qry)
-            for qry in split_sql_str(query)
-            if qry
-        )
+        conn.prepare(qry)
+        for qry in split_sql_str(query)
+        if qry
     )
+    yield renderer.render_intro()
     try:
         with conn.xact():
-            yield from resp_func(statements)
-    finally:
-        conn.close()
+            for stmt in statements:
+                if stmt.column_names:
+                    query_renderer = renderer.get_query_renderer(
+                        stmt.column_names,
+                        stmt.pg_column_types
+                    )
+                    yield query_renderer.render_intro()
+                    try:
+                        for rows in stmt.chunks(*args):
+                            yield query_renderer.render_rows(rows)
+                    finally:
+                        yield query_renderer.render_outro()
+                else:
+                    yield renderer.render_nonquery(stmt.first(*args))
+    except Exception as ex:
+        yield renderer.render_exception(ex)
+    yield renderer.render_outro()
+    conn.close()
 
 
-def json_response(statements):
-    stmt = next(statements)
-    get_result, colnames, _ = stmt
-    yield json_dumps(
-        [dict(zip(colnames, row))
-        for rows in get_result()
-        for row in rows],
-        ensure_ascii=False
-    )
-
-
-def yield_per_none(fun):
+def strjoin(fun):
     def wrapper(*args, **kw):
-        buff = []
-        for item in fun(*args, **kw):
-            if item is None:
-                yield ''.join(buff)
-                buff = []
-            else:
-                buff.append(item)
-        yield ''.join(buff)
+        return ''.join(fun(*args, **kw))
     return wrapper
 
 
-@yield_per_none
-def map_response(statements):
-    yield ('<html>'
-        '<head>'
-        '<link href="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet.css" rel="stylesheet" type="text/css" />'
-        '<script>L_PREFER_CANVAS = true;</script>'
-        '<script src="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet.js"></script>'
-        '<link href="/map.css" rel="stylesheet" type="text/css" />'
-        '</head>'
-        '<body>'
-        '<div id="map"></div>'
-        '<script src="/map.js"></script>')
-    yield None # flush
+class RegularRenderer:
+    mime_type = 'text/html'
 
-    for i, (get_result, colnames, coltypes) in enumerate(statements):
-        geomcol_ix = colnames.index('geom')
-        propnames = colnames[:geomcol_ix] + colnames[geomcol_ix+1:]
-        for rows in get_result():
-            yield '<script>addFeatureCollection('
-            yield str(i)
-            yield ','
-            yield json_dumps({
-                'type': 'FeatureCollection',
-                'features': [{
-                    'type': 'Feature',
-                    'geometry': json_loads(row[geomcol_ix] or 'null'),
-                    'properties': dict(zip(
-                        propnames,
-                        (x for i, x in enumerate(row)
-                            if i != geomcol_ix)
-                    ))
-                } for row in rows]
-            }, ensure_ascii=False)
-            yield ')</script>'
-            yield None # flush
+    def render_intro(self):
+        return (
+            '<html>'
+            '<head>'
+            '<link href="/table.css" rel="stylesheet" type="text/css" />'
+            '</head>'
+            '<body>')
 
-    yield ('</body>'
-        '</html>')
+    def render_outro(self):
+        return (
+            '</body>'
+            '</html>')
+
+    def render_exception(self, exception):
+        return '<pre class="error">{0}</pre>'.format(exception)
+
+    def render_nonquery(self, result):
+        return '<p class="non-query-result">{0}</p>'.format(result)
+
+    def get_query_renderer(self, *args, **kw):
+        return TableRenderer(*args, **kw)
 
 
-@yield_per_none
-def html_response(statements):
-    yield '<html>'
-    yield '<head>'
-    yield '<link href="/table.css?2" rel="stylesheet" type="text/css" />'
-    yield '</head>'
-    yield '<body>'
-
-    exception = None
-    has_statements = True
-    statements = iter(statements)
-    while has_statements and not exception:
-        try:
-            get_result, colnames, coltypes = next(statements)
-        except StopIteration:
-            has_statements = False
-            continue
-        except Exception as ex:
-            exception = ex
-            continue
-
-        # execute non query
-        if not colnames:
-            try:
-                nonqry_result = get_result()
-            except Exception as ex:
-                exception = ex
-            else:
-                yield '<p class="non-query-result">'
-                yield str(nonqry_result)
-                yield '</p>'
-                yield None # flush
-            continue
-
-        # execute select
-        yield '<table>'
-        yield '<tr>'
-        for name in colnames:
-            yield '<th>'
-            yield name
-            yield '</th>'
-        yield '</tr>'
-        yield None # flush
-
-        col_classes = [
+class TableRenderer:
+    def __init__(self, colnames, coltypes):
+        self.colnames = colnames
+        self.coltypes = coltypes
+        self.colclasses = [
             'int' if typ == 23 else 'str'
             for typ in coltypes
         ]
 
-        chunks = get_result()
-        has_chunks = True
-        while has_chunks and not exception:
-            try:
-                rows = next(chunks)
-            except StopIteration:
-                has_chunks = False
-            except Exception as ex:
-                exception = ex
+    @strjoin
+    def render_intro(self):
+        yield '<table>'
+        yield '<tr>'
+        for name in self.colnames:
+            yield '<th>'
+            yield name
+            yield '</th>'
+        yield '</tr>'
+
+    def render_outro(self):
+        return '</table>'
+
+    @strjoin
+    def render_rows(self, rows):
+        for row in rows:
+            yield '<tr>'
+            for val, class_ in zip(row, self.colclasses):
+                yield '<td class="' + class_ + '">'
+                yield '<em>null</em>' if val is None else str(val)[:100]
+                yield '</td>'
+            yield '</tr>'
+
+
+class MapRenderer:
+    mime_type = 'text/html'
+
+    def __init__(self):
+        self._querynum = 0
+
+    def render_intro(self):
+        return (
+            '<html>'
+            '<head>'
+            '<link href="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet.css" rel="stylesheet" type="text/css" />'
+            '<script>L_PREFER_CANVAS = true;</script>'
+            '<script src="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet.js"></script>'
+            '<link href="/map.css" rel="stylesheet" type="text/css" />'
+            '</head>'
+            '<body>'
+            '<div id="map"></div>'
+            '<script src="/map.js"></script>')
+
+    def render_outro(self):
+        return (
+            '</body>'
+            '</html>')
+
+    def render_exception(self, exception):
+        return '<script>console.error({0!r})</script>'.format(
+            str(exception)
+        )
+
+    def render_nonquery(self, result):
+        return '<script>console.log({0!r})</script>'.format(
+            str(result)
+        )
+
+    def get_query_renderer(self, *args, **kw):
+        self._querynum += 1
+        return GeoJsonRenderer(*args, num=self._querynum, **kw)
+
+
+class GeoJsonRenderer:
+    def __init__(self, colnames, coltypes, num):
+        self.colnames = colnames
+        self.coltypes = coltypes
+        self.num = num
+        self.geomcol_ix = geomcol_ix = colnames.index('geom')
+        self.propnames = colnames[:geomcol_ix] + colnames[geomcol_ix+1:]
+
+    def render_intro(self):
+        return ''
+
+    def render_outro(self):
+        return ''
+
+    @strjoin
+    def render_rows(self, rows):
+        yield '<script>addFeatureCollection('
+        yield str(self.num)
+        yield ','
+        yield json_dumps({
+            'type': 'FeatureCollection',
+            'features': [{
+                'type': 'Feature',
+                'geometry': json_loads(row[self.geomcol_ix] or 'null'),
+                'properties': dict(zip(
+                    self.propnames,
+                    (x for i, x in enumerate(row)
+                        if i != self.geomcol_ix)
+                ))
+            } for row in rows]
+        }, ensure_ascii=False)
+        yield ')</script>'
+
+
+class JsonRenderer:
+    mime_type = 'application/json'
+
+    def render_intro(self):
+        return ''
+
+    def render_outro(self):
+        return ''
+
+    def render_exception(self, exception):
+        raise exception
+
+    def render_nonquery(self, result):
+        return ''
+
+    def get_query_renderer(self, *args, **kw):
+        return JsonQueryRenderer(*args, **kw)
+
+class JsonQueryRenderer:
+    def __init__(self, colnames, coltypes):
+        self.colnames = colnames
+        self.coltypes = coltypes
+        self.first = True
+
+    def render_intro(self):
+        return '['
+
+    def render_outro(self):
+        return ']'
+
+    @strjoin
+    def render_rows(self, rows):
+        for row in rows:
+            if self.first:
+                self.first = False
             else:
-                for row in rows:
-                    yield '<tr>'
-                    for val, class_ in zip(row, col_classes):
-                        yield '<td class="' + class_ + '">'
-                        if val is None:
-                            yield '<em>null</em>'
-                        else:
-                            yield str(val)[:100]
-                        yield '</td>'
-                    yield '</tr>'
-                yield None # flush
+                yield ','
+            yield json_dumps(
+                dict(zip(self.colnames, row)),
+                ensure_ascii=False
+            )
 
-        yield '</table>'
 
-    if exception:
-        yield '<pre class="error">'
-        yield str(exception)
-        yield '</pre>'
-
-    yield '</body>'
-    yield '</html>'
+RENDERERS = {
+    'map': MapRenderer(),
+    'html': RegularRenderer(),
+    'json': JsonRenderer(),
+}
