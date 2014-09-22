68b4447a0fdc4a661fb2580fc5c2f269d023c08f
exe-dealer <exe-dealer@yandex.ru>
2014-09-22 21:45:24 +0400

    drop function in def

--- pgblackboard/sql/func_def.sql
+++ pgblackboard/sql/func_def.sql
@@ -1,1 +1,10 @@
-select pg_get_functiondef(%(oid)s::int::regproc) as def
+select concat_ws(e'\n'
+    ,format('-- DROP FUNCTION %%s(%%s);'
+        ,oid::regproc
+        ,array_to_string(proargtypes::regtype[], ', ')
+    )
+    ,''
+    ,pg_get_functiondef(oid)
+) as def
+from pg_proc as p
+where oid = %(oid)s
