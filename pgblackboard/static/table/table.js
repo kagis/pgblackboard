8850a0ba87b6ec8c5cdc631cc1fed1bd2f143a25
exe-dealer <exe-dealer@yandex.ru>
2014-09-28 00:06:08 +0400

    fix issue #22 row submitting indication

--- pgblackboard/static/table/table.js
+++ pgblackboard/static/table/table.js
@@ -18,15 +18,28 @@ window.addEventListener('click', function (e) {
 // to reduce response size
 window.addEventListener('click', function (e) {
     if (e.target.nodeName === 'TD'
+
+        // skip if already editable
         && e.target.contentEditable !== 'plaintext-only'
+
+        // not allow focus when row is submitting
+        && !e.target.parentNode/*TR*/.classList.contains('submitting-row')
+
+        // not allow focus on row header
         && e.target.cellIndex > 0
-        && e.target.parentNode/*TR*/.parentNode/*TBODY*/.parentNode/*TABLE*/.dataset.table /* is editable */)
+
+        // can focus only when table is updatable/insertable
+        && e.target.parentNode/*TR*/.parentNode/*TBODY*/.parentNode/*TABLE*/.dataset.table)
     {
         var cells = e.target.parentNode.cells;
-        for (var i = 1 /* skip # cell */; i < cells.length; i++) {
+        for (var i = 1 /* skip row header */; i < cells.length; i++) {
             var cell = cells[i];
             cell.contentEditable = 'plaintext-only';
-            cell.dataset.origval = cell.textContent;
+
+            // save original values
+            if (!('origval' in cell.dataset)) {
+                cell.dataset.origval = cell.textContent;
+            }
         }
         e.target.focus();
     }
@@ -36,12 +49,12 @@ window.addEventListener('click', function (e) {
 // fix blank row when input
 window.addEventListener('input', function (e) {
     if (e.target.nodeName === 'TD') {
-        var row = e.target.parentNode;
-        var tBody = row.parentNode;
-        if (tBody.classList.contains('has-blankrow') && tBody.lastChild === row) {
-            row.dataset.inserting = true;
+        var editingRow = e.target.parentNode;
+        var tBody = editingRow.parentNode;
+        if (tBody.classList.contains('has-blankrow') && tBody.lastChild === editingRow) {
+            editingRow.dataset.inserting = true;
             var newBlankRow = document.createElement('TR');
-            for (var i = 0; i < row.cells.length; i++) {
+            for (var i = 0; i < editingRow.cells.length; i++) {
                 newBlankRow.appendChild(document.createElement('TD'));
             }
             tBody.appendChild(newBlankRow);
@@ -51,34 +64,43 @@ window.addEventListener('input', function (e) {
         var cellWasDirty = !!cell.dataset.dirty;
         var cellNowDirty = (cell.dataset.origval !== cell.textContent);
         cell.dataset.dirty = cellNowDirty || '';
-        row.dataset.dirtycells = (+(row.dataset.dirtycells || 0) + (cellNowDirty - cellWasDirty)) || '';
-
-        row.classList.remove('invalid-row');
+        editingRow.dataset.dirtycells = (+(editingRow.dataset.dirtycells || 0) + (cellNowDirty - cellWasDirty)) || '';
     }
 });
 
 
 // submit dirty row when switch to another row
 window.addEventListener('blur', function (e) {
-    if (e.target.nodeName === 'TD'
-        && e.target.parentNode/*TR*/.dataset.dirtycells)
-    {
-        var blurredEl = e.target;
+    if (e.target.nodeName === 'TD') {
+        var blurredRow = e.target.parentNode/*TR*/;
+
+        // disable editing on blurred row
+        for (var i = blurredRow.cells.length - 1; i >= 1; i--) {
+            blurredRow.cells[i].contentEditable = false;
+        };
+
         setTimeout(function () {
             var focusedEl = document.activeElement;
-            if (blurredEl.parentNode/*TR*/ !== focusedEl.parentNode/*TR*/) {
-                submitDirtyRow(blurredEl.parentNode);
+            if (blurredRow !== focusedEl.parentNode/*TR*/) {
+                submitDirtyRow(blurredRow);
             }
         }, 10);
     }
 }, true);
 
 
-
 function submitDirtyRow(row) {
+    row.classList.remove('invalid-row');
+
+    if (!row.dataset.dirtycells) {
+        return;
+    }
+
     var cells = row.cells;
     var table = row.parentNode/*TBODY*/.parentNode/*TABLE*/;
     var headcells = table.tHead.rows[0].cells;
+
+    row.classList.add('submitting-row');
 
     var changes = {};
     var key = {};
@@ -96,28 +118,8 @@ function submitDirtyRow(row) {
     var req = new XMLHttpRequest();
     req.open('POST', 'edit');
     req.responseType = 'json';
-
-    req.onload = function (e) {
-        if (e.target.status === 200) {
-            row.classList.remove('invalid-row');
-            delete row.dataset.inserting;
-            delete row.dataset.dirtycells;
-            for (var i = 1; i < cells.length; i++) {
-                var cell = cells[i]
-                delete cell.dataset.origval;
-                delete cell.dataset.dirty;
-                cell.contentEditable = false;
-                var returnedVal = e.target.response[headcells[i].dataset.name]
-                if (returnedVal !== undefined) {
-                    cell.textContent = returnedVal;
-                }
-            }
-        } else {
-            row.classList.add('invalid-row');
-            alert(e.target.response);
-        }
-    };
-
+    req.onloadend = onLoadEnd;
+    req.onload = onLoad;
     req.send(JSON.stringify({
         action   : row.dataset.inserting ? 'insert' : 'update',
         database : table.dataset.database,
@@ -126,4 +128,36 @@ function submitDirtyRow(row) {
         changes  : changes,
         where    : key
     }));
+
+
+    function onLoad(e) {
+        if (e.target.status === 200) {
+
+            // row is not dirty since it successfully submitted
+            delete row.dataset.dirtycells;
+            delete row.dataset.inserting;
+            row.classList.remove('invalid-row');
+
+            for (var i = 1; i < cells.length; i++) {
+                var cell = cells[i]
+
+                // invalidate original value
+                delete cell.dataset.origval;
+                delete cell.dataset.dirty;
+
+                // refresh cell with actual inserted/updated value
+                var returnedVal = e.target.response[headcells[i].dataset.name];
+                if (returnedVal !== undefined) {
+                    cell.textContent = returnedVal;
+                }
+            }
+        } else {
+            row.classList.add('invalid-row');
+            alert(e.target.response);
+        }
+    }
+
+    function onLoadEnd() {
+        row.classList.remove('submitting-row');
+    }
 }
