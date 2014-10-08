05db03aba00fb5030b13a687f75fc6f7958b5c20
exe-dealer <exe-dealer@yandex.ru>
2014-10-08 23:54:24 +0400

    gzip static

--- pgblackboard/fileapp.py
+++ pgblackboard/fileapp.py
@@ -1,17 +1,34 @@
-import pkgutil, mimetypes
+import pkgutil, mimetypes, gzip
 
 
 class ResourceFileApp:
     def __init__(self, path):
+
+        cache_control = 'max-age=86400, public' # 1 day cache
+
+        contenttype, _ = mimetypes.guess_type(path)
+        if contenttype in ('application/javascript', 'text/css'):
+            contenttype += '; charset=utf-8'
+
         self._content = pkgutil.get_data('pgblackboard', path)
-        self._contenttype, _ = mimetypes.guess_type(path)
-        if type(self._content) is str:
-            self._contenttype += '; charset=utf-8'
-            self._content = self._content.encode()
+        self._headers = (
+            ('Content-Type', contenttype),
+            ('Content-Length', str(len(self._content))),
+            ('Cache-Control', cache_control)
+        )
+
+        self._gzipped_content = gzip.compress(self._content)
+        self._gzipped_headers = (
+            ('Content-Type', contenttype),
+            ('Content-Length', str(len(self._gzipped_content))),
+            ('Cache-Control', cache_control),
+            ('Content-Encoding', 'gzip')
+        )
 
     def __call__(self, environ, start_response):
-        start_response('200 OK', [
-            ('Content-type', self._contenttype),
-            ('Cache-Control', 'max-age=86400, public')
-        ])
-        yield self._content
+        if 'gzip' in environ.get('HTTP_ACCEPT_ENCODING', '').split(','):
+            start_response('200 OK', self._gzipped_headers)
+            yield self._gzipped_content
+        else:
+            start_response('200 OK', self._headers)
+            yield self._content
