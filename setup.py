3e363057e7fdfe89457995fd7b411fc766576c5d
exe-dealer <exe-dealer@yandex.ru>
2014-12-22 12:53:18 +0300

    setup distutils

--- setup.py
+++ setup.py
@@ -18,4 +18,36 @@ setup(
     keywords=['PostgreSQL', 'Postgres'],
     include_package_data=True,
     test_suite='test',
+    package_data={ 'pgblackboard': [
+         'index.html'
+
+        ,'static/favicon.ico'
+        ,'static/app.js'
+        ,'static/app.css'
+        ,'static/table.js'
+        ,'static/table.css'
+        ,'static/map.js'
+        ,'static/map.css'
+        ,'static/queryplan.js'
+        ,'static/queryplan.css'
+
+        ,'static/codemirror-pgsql.js'
+
+        ,'static/lib/codemirror/4.6.0/codemirror.js'
+        ,'static/lib/codemirror/4.6.0/mode/sql/sql.js'
+        ,'static/lib/codemirror/4.6.0/addon/search/searchcursor.js'
+        ,'static/lib/codemirror/4.6.0/keymap/sublime.js'
+        ,'static/lib/codemirror/4.6.0/addon/edit/matchbrackets.js'
+        ,'static/lib/codemirror/4.6.0/addon/edit/closebrackets.js'
+        ,'static/lib/codemirror/4.6.0/codemirror.css'
+        ,'static/lib/knockout/3.2.0/knockout.js'
+        ,'static/lib/knockout-flatBindingProvider/1.0.0/knockout-flatBindingProvider.js'
+        ,'static/lib/d3/3.4.11/d3.js'
+        ,'static/lib/dagre-d3/0.2.9/dagre-d3.js'
+        ,'static/lib/leaflet/0.7.3/leaflet.js'
+        ,'static/lib/leaflet/0.7.3/leaflet.css'
+
+        ,'sql/def/*.sql'
+        ,'sql/children/*.sql'
+    ]},
 )
