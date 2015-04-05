d66894c47fa0cd4e8d8e1e608ae2835c2683a446
exe-dealer <exe-dealer@yandex.ru>
2015-04-05 14:15:20 +0300

    view

--- src/view.rs
+++ src/view.rs
@@ -1,7 +1,10 @@
 use std::io::{self, Write};
 
 pub trait View {
-    //fn new<TWriter: Writer>(writer: TWriter) -> Self;
+
+    fn render_intro(&mut self) -> io::Result<()>;
+
+    fn render_outro(&mut self) -> io::Result<()>;
 
     fn render_rowset_begin(&mut self,
                            rowset_id: i32,
@@ -13,13 +16,6 @@ pub trait View {
     fn render_row<'a, T>(&mut self, row: T) -> io::Result<()>
         where T: Iterator<Item=Option<&'a str>>;
 
-    fn make_rowset_editable(&mut self,
-                            rowset_id: i32,
-                            table_name: &str,
-                            col_names: &[&str],
-                            pk_mask: &[bool])
-                            -> io::Result<()>;
-
     fn render_notice(&mut self, message: &str) -> io::Result<()>;
 
     fn render_sql_error(&mut self,
@@ -30,6 +26,13 @@ pub trait View {
     fn render_io_error(&mut self, io::Error) -> io::Result<()>;
 
     fn render_nonquery(&mut self, command_tag: &str) -> io::Result<()>;
+
+    fn make_rowset_editable(&mut self,
+                            rowset_id: i32,
+                            table_name: &str,
+                            col_names: &[&str],
+                            pk_mask: &[bool])
+                            -> io::Result<()>;
 }
 
 pub struct FieldDescription<'a, 'b> {
@@ -39,20 +42,57 @@ pub struct FieldDescription<'a, 'b> {
 }
 
 
+static INTRO_START: &'static [u8] = b"\
+    <!DOCTYPE html>\
+    <html>\
+    <head>\
+        <meta charset='utf-8' />\
+        <title></title>\
+";
+
+static INTRO_END: &'static [u8] = b"\
+    </head>\
+    <body>\
+        <script>frameElement.setupPgBlackboardOutputFrame();</script>\
+        <div class='main'>\
+";
 
 
-struct TableView<T: Write>(T);
+pub struct TableView<W: Write> {
+    writer: W
+}
+
+impl<W: Write> TableView<W> {
+    pub fn new(writer: W) -> TableView<W> {
+        TableView { writer: writer }
+    }
+}
 
 impl<W: Write> View for TableView<W> {
+
+    fn render_intro(&mut self) -> io::Result<()> {
+        let ref mut writer = self.writer;
+        try!(writer.write_all(INTRO_START));
+        try!(writer.write_all(b"\
+            <link href='table.css' rel='stylesheet' />\
+            <script src='table.js' async='async'></script>"));
+        try!(writer.write_all(INTRO_END));
+        Ok(())
+    }
+
+    fn render_outro(&mut self) -> io::Result<()> {
+        self.writer.write_all(b"</div></body></html>")
+    }
 
     fn render_rowset_begin(&mut self,
                            rowset_id: i32,
                            cols_descr: &[FieldDescription])
                            -> io::Result<()>
     {
-        let out = &mut self.0;
+        let out = &mut self.writer;
 
-        let numeric_cols = cols_descr.iter().enumerate()
+        let numeric_cols = cols_descr.iter()
+                                     .enumerate()
                                      .filter(|&(i, col)| col.is_numeric)
                                      .map(|(i, col)| i + 1);
 
@@ -74,15 +114,15 @@ impl<W: Write> View for TableView<W> {
                         {coltype}\
                     </small>\
                 <th>",
-                colname=col.name,
-                coltype=col.typ));
+                colname = col.name,
+                coltype = col.typ));
         }
         try!(out.write_all(b"</tr>"));
         Ok(())
     }
 
     fn render_rowset_end(&mut self) -> io::Result<()> {
-        let writer = &mut self.0;
+        let writer = &mut self.writer;
         try!(writer.write_all(b"</table>"));
         Ok(())
     }
@@ -94,13 +134,17 @@ impl<W: Write> View for TableView<W> {
                             pk_mask: &[bool])
                             -> io::Result<()>
     {
+        try!(write!(&mut self.writer, "<script>pgBlackboard\
+                                .makeRowsetEditable();\
+                                </script>"));
+
         Ok(())
     }
 
     fn render_row<'a, T>(&mut self, row: T) -> io::Result<()>
         where T: Iterator<Item=Option<&'a str>> {
 
-        let writer = &mut self.0;
+        let writer = &mut self.writer;
         try!(writer.write_all(b"<tr>"));
         try!(writer.write_all(b"<th></th>"));
         for maybe_val in row {
@@ -115,7 +159,7 @@ impl<W: Write> View for TableView<W> {
     }
 
     fn render_io_error(&mut self, err: io::Error) -> io::Result<()> {
-        write!(&mut self.0, "<pre>{:?}</pre>", err)
+        write!(&mut self.writer, "<pre>{:?}</pre>", err)
     }
 
     fn render_sql_error(&mut self,
@@ -123,7 +167,8 @@ impl<W: Write> View for TableView<W> {
                         script_line: Option<usize>)
                         -> io::Result<()>
     {
-        let writer = &mut self.0;
+        let writer = &mut self.writer;
+        try!(invoke_js_set_error(writer, message, script_line.unwrap_or(0)));
         try!(writer.write_all(b"<pre>"));
         try!(writer.write_all(message.as_bytes()));
         try!(writer.write_all(b"</pre>"));
@@ -131,7 +176,7 @@ impl<W: Write> View for TableView<W> {
     }
 
     fn render_notice(&mut self, message: &str) -> io::Result<()> {
-        let writer = &mut self.0;
+        let writer = &mut self.writer;
         try!(writer.write_all(b"<pre>"));
         try!(writer.write_all(message.as_bytes()));
         try!(writer.write_all(b"</pre>"));
@@ -139,7 +184,7 @@ impl<W: Write> View for TableView<W> {
     }
 
     fn render_nonquery(&mut self, command_tag: &str) -> io::Result<()> {
-        let writer = &mut self.0;
+        let writer = &mut self.writer;
         try!(writer.write_all(b"<pre>"));
         try!(writer.write_all(command_tag.as_bytes()));
         try!(writer.write_all(b"</pre>"));
@@ -148,15 +193,51 @@ impl<W: Write> View for TableView<W> {
 }
 
 
-fn main() {
-    let mut view = TableView(io::stdout());
+// pub struct MapView<W: Write> {
+//     inner: W
+// }
 
+// impl<W: Write> View for MapView<W> {
+
+// }
+
+
+
+fn invoke_js_set_error<W>(writer: &mut W,
+                          message: &str,
+                          script_line: usize)
+                          -> io::Result<()>
+                          where W: Write
+{
+    write!(writer,
+       "<script>pgBlackboard.setError('{message}', {line});</script>",
+       message = message.replace("'", "\\'"),
+       line = script_line)
+}
+
+fn main() {
+    let mut view = TableView::new(io::stdout());
+
+    view.render_intro().unwrap();
     view.render_rowset_begin(1, &[
         FieldDescription { name: "one", typ: "text", is_numeric: false },
         FieldDescription { name: "two", typ: "int", is_numeric: true },
     ]);
-    view.render_row(vec![
-        Some("foo"),
-        Some("bar"),
-    ].into_iter()).unwrap();
+
+    let row = vec![
+        Some("foo".to_string()),
+        Some("bar".to_string()),
+    ];
+
+    let row_iter = row.iter().map(|maybe_val| {
+        maybe_val.as_ref().map(|val| &val[..])
+    });
+
+    view.render_row(row_iter).unwrap();
+    view.render_rowset_end().unwrap();
+
+    view.render_sql_error("err message", Some(1)).unwrap();
+
+    view.render_outro().unwrap();
 }
+
